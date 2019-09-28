84c7d187014ff8cf93b20b66f92786378fa65de7
Albert Stjärne <albert.stjarne@gmail.com>
2019-09-28 10:34:12 +0200

    adds styling

--- src/DogDetails.js
+++ src/DogDetails.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-// import './DogDetails.css';
+import './DogDetails.css';
 import { Link } from 'react-router-dom';
 
 class DogDetails extends Component {
